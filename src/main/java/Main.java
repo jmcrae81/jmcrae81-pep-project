953e389e687ddc07872662e60359d9eafccfe792
jmcrae81 <jimmy.a.mcrae@gmail.com>
2024-10-02 20:24:20 -0400

    Changes to be committed: modified: ../Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
 import Controller.SocialMediaController;
 import io.javalin.Javalin;
 
+import Model.Account;
+import Model.Message;
+import DAO.SocialBlogDAO;
+
+
 /**
  * This class is provided with a main method to allow you to manually run and test your application. This class will not
  * affect your program in any way and you may write whatever code you like here.
@@ -10,5 +15,16 @@ public class Main {
         SocialMediaController controller = new SocialMediaController();
         Javalin app = controller.startAPI();
         app.start(8080);
+
+        SocialBlogDAO sbDAO = new SocialBlogDAO();
+
+        Account newUser = new Account("newUser", "newpass");
+
+        if(newUser != null){
+            System.out.println("Success!");
+        }else{
+            System.out.println("Failed to create new account!");
+        }            
+
     }
 }
